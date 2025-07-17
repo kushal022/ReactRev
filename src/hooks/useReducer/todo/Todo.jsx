342638960d9b50957288e6d4254e144578094678
kushal022 <kushla.learn22@gmail.com>
2025-07-17 10:46:13 +0530

    todos logic complete with useReduser hook

--- src/hooks/useReducer/todo/Todo.jsx
+++ src/hooks/useReducer/todo/Todo.jsx
@@ -1,104 +1,130 @@
-import React, { useReducer, useState } from 'react'
-import { todoReducer } from './todoReducer'
+import React, { useEffect, useReducer, useState } from "react";
+import { todoReducer } from "./todoReducer";
+
+const initialValue = () => {
+  let data = localStorage.getItem("todos");
+  return data ? JSON.parse(data) : [];
+};
 
 const Todo = () => {
-    const [text, setText] = useState('')
-    const [todos, dispatch] = useReducer(todoReducer,[])
-    const [isEdit, setIsEdit] = useState(false)
+  const [text, setText] = useState("");
+  const [todos, dispatch] = useReducer(todoReducer, [], initialValue);
+  const [isEdit, setIsEdit] = useState(false);
 
-    const handleAdd = ()=>{
-        if(text.trim()==='')return;
-        dispatch({type:'add',payload:text})
-        setText('')
-    }
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
-    const handleDelete = (id)=>{
-        dispatch({type: 'delete',payload:id})
-    }
+  const handleAdd = () => {
+    if (text.trim() === "") return;
+    dispatch({ type: "add", payload: text });
+    setText("");
+  };
 
-    const handleCheck = (id)=>{
-        dispatch({type: 'check', payload:id})
-    }
+  const handleDelete = (id) => {
+    dispatch({ type: "delete", payload: id });
+  };
 
-    const handleClear = ()=>{
-        dispatch({type:'clearCompleted'})
-    }
+  const handleCheck = (id) => {
+    dispatch({ type: "check", payload: id });
+  };
 
-    const handleEdit = (id,todoText)=>{
-        setText(todoText)
-        setIsEdit(true)
-        dispatch({type:'edit',payload:id})
-    }
+  const handleClear = () => {
+    dispatch({ type: "clearCompleted" });
+  };
 
-    const handleUpdate = ()=>{
-        dispatch({type:'update',payload:text})
-        setIsEdit(false)
-        setText('')
-    }
+  const handleEdit = (id, todoText) => {
+    setText(todoText);
+    setIsEdit(true);
+    dispatch({ type: "edit", payload: id });
+  };
 
-    const handleCancel = ()=>{
-        setIsEdit(false)
-        setText('')
-        dispatch({type:'cancel'})
-    }
+  const handleUpdate = () => {
+    dispatch({ type: "update", payload: text });
+    setIsEdit(false);
+    setText("");
+  };
 
-    const completedTodos = todos.filter(todo=>todo.isCompleted===true)
-    const pendingTodos = todos.filter(todo=>todo.isCompleted!==true)
+  const handleCancel = () => {
+    setIsEdit(false);
+    setText("");
+    dispatch({ type: "cancel" });
+  };
+  console.log(todos);
+  const completedTodos =
+    todos && todos.filter((todo) => todo.isCompleted === true);
+  const pendingTodos =
+    todos && todos.filter((todo) => todo.isCompleted !== true);
   return (
     <div>
-        <h5>Todo with useReducer()</h5>
-        <div>
-            <input type="text" value={text}
-            onChange={e=>setText(e.target.value)}
-            placeholder='enter todo..' />
-            {!isEdit ?
-                <button onClick={handleAdd} >Add</button> :
-                <div>
-                    <button onClick={handleUpdate} >update</button> 
-                    <button onClick={handleCancel} >Cancel</button> 
-                </div>
-            }
-        </div>
-        <p>All Tasks:</p>
-        <ul>
-            {todos&&todos.map(todo=>(
-                <li key={todo.id}>
-                    <span style={{color:todo.isCompleted?'green':'black'}} >{todo.text}</span>
-                    <button onClick={()=>handleCheck(todo.id)}>{todo.isCompleted?'UnCheck':'Check'}</button>
-                    <button onClick={()=>handleEdit(todo.id,todo.text)}>Edit</button>
-                    <button onClick={()=>handleDelete(todo.id)}>Delete</button>
-                </li>
-            ))}
-        </ul>
-        <p>Pending Tasks:</p>
-        <ul>
-            {
-                pendingTodos.map(todo=>(
-                    <li key={todo.id}>
-                    <span style={{color:todo.isCompleted?'green':'black'}} >{todo.text}</span>
-                    <button onClick={()=>handleCheck(todo.id)}>{todo.isCompleted?'UnCheck':'Check'}</button>
-                    <button onClick={()=>handleEdit(todo.id,todo.text)}>Edit</button>
-                    <button onClick={()=>handleDelete(todo.id)}>Delete</button>
-                </li>
-                ))
-            }
-        </ul>
-        <p>Completed Tasks:</p>
-        <ul>
-            {
-                completedTodos.map(todo=>(
-                    <li key={todo.id}>
-                    <span style={{color:todo.isCompleted?'green':'black'}} >{todo.text}</span>
-                    <button onClick={()=>handleCheck(todo.id)}>{todo.isCompleted?'UnCheck':'Check'}</button>
-                    <button onClick={()=>handleEdit(todo.id,todo.text)}>Edit</button>
-                    <button onClick={()=>handleDelete(todo.id)}>Delete</button>
-                </li>
-                ))
-            }
-        </ul>
-        <button onClick={handleClear} >Clear Completed</button>
+      <h5>Todo with useReducer()</h5>
+      <div>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="enter todo.."
+        />
+        {!isEdit ? (
+          <button onClick={handleAdd}>Add</button>
+        ) : (
+          <div>
+            <button onClick={handleUpdate}>update</button>
+            <button onClick={handleCancel}>Cancel</button>
+          </div>
+        )}
+      </div>
+      <p>All Tasks:</p>
+      <ul>
+        {todos &&
+          todos.map((todo) => (
+            <li key={todo.id}>
+              <span style={{ color: todo.isCompleted ? "green" : "black" }}>
+                {todo.text}
+              </span>
+              <button onClick={() => handleCheck(todo.id)}>
+                {todo.isCompleted ? "UnCheck" : "Check"}
+              </button>
+              <button onClick={() => handleEdit(todo.id, todo.text)}>
+                Edit
+              </button>
+              <button onClick={() => handleDelete(todo.id)}>Delete</button>
+            </li>
+          ))}
+      </ul>
+      <p>Pending Tasks:</p>
+      <ul>
+        {pendingTodos.map((todo) => (
+          <li key={todo.id}>
+            <span style={{ color: todo.isCompleted ? "green" : "black" }}>
+              {todo.text}
+            </span>
+            <button onClick={() => handleCheck(todo.id)}>
+              {todo.isCompleted ? "UnCheck" : "Check"}
+            </button>
+            <button onClick={() => handleEdit(todo.id, todo.text)}>Edit</button>
+            <button onClick={() => handleDelete(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <p>Completed Tasks:</p>
+      <ul>
+        {completedTodos.map((todo) => (
+          <li key={todo.id}>
+            <span style={{ color: todo.isCompleted ? "green" : "black" }}>
+              {todo.text}
+            </span>
+            <button onClick={() => handleCheck(todo.id)}>
+              {todo.isCompleted ? "UnCheck" : "Check"}
+            </button>
+            <button onClick={() => handleEdit(todo.id, todo.text)}>Edit</button>
+            <button onClick={() => handleDelete(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleClear}>Clear Completed</button>
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
