585424c0a7364c334c02d2cd6db21484361cca32
kushal022 <kushla.learn22@gmail.com>
2025-07-19 11:16:25 +0530

    nested redux-persist exclude counter and pass todo slice reducer

--- redux0.1/src/app/store.js
+++ redux0.1/src/app/store.js
@@ -1,6 +1,7 @@
+//^nested redux-persist:
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from '../features/counter/counterSlice'
-import todoReducer from '../features/todos/todoSlice'
+import counterReducer from "../features/counter/counterSlice";
+import todoReducer from "../features/todos/todoSlice";
 
 //^ Without LocalStorage:
 // export const  store = configureStore({
@@ -11,22 +12,77 @@ import todoReducer from '../features/todos/todoSlice'
 // })
 
 //^ Implement LocalStorage: by Redux-persist
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // localStorage
-import { combineReducers } from 'redux';
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // localStorage
+import { combineReducers } from "redux";
+import { todosPersistConfig } from "./todoPersistConfig";
 
-const rootReducer = combineReducers({ todos: todoReducer, counter: counterReducer });
+const rootReducer = combineReducers({
+  todos: persistReducer(todosPersistConfig, todoReducer), // todo persist
+  counter: counterReducer, // not persisted
+});
 
-const persistConfig = { key: 'root', storage };
+//& Persist/Store individual/Selected Slice:
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["counter"],
+}; // only 'todos' will be persisted/stored in localStorage
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: (gdm) =>
-    gdm({ serializableCheck: { ignoredActions: [
-      'persist/PERSIST', 'persist/REHYDRATE'
-    ] } }),
+  middleware: (
+    gdm // gdm= getDefaultMiddleware
+  ) =>
+    gdm({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
+
+//^--------- none nested -----------------
+// import { configureStore } from "@reduxjs/toolkit";
+// import counterReducer from '../features/counter/counterSlice'
+// import todoReducer from '../features/todos/todoSlice'
+
+//^ Without LocalStorage:
+// export const  store = configureStore({
+//     reducer: {
+//         counter: counterReducer,
+//         todos: todoReducer
+//     }
+// })
+
+//^ Implement LocalStorage: by Redux-persist
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage'; // localStorage
+// import { combineReducers } from 'redux';
+
+// const rootReducer = combineReducers({ todos: todoReducer, counter: counterReducer });
+
+//& Persist/Store All Slices:
+/// const persistConfig = { key: 'root', storage };
+//& Persist/Store individual/Selected Slice:
+// const persistConfig = {
+//     key: 'root',
+//     storage,
+//     whitelist: ['todos'], // or
+    // blacklist: ['counter']
+// }; // only 'todos' will be persisted/stored in localStorage
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// export const store = configureStore({
+//   reducer: persistedReducer,
+//   middleware: (gdm) =>
+//     gdm({ serializableCheck: { ignoredActions: [
+//       'persist/PERSIST', 'persist/REHYDRATE'
+//     ] } }),
+// });
+
+// export const persistor = persistStore(store);
